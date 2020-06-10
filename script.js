e9afc2876fed5b73f10c931c7a0705cc43e9c2ab
Dale Foskey <dalefoskey@icloud.com>
2020-06-10 09:18:43 -0400

    updated javascript

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
+// prompt to get users name for high scores area
 let userName = prompt('What is your first name? After you click ok, the quiz will start.');
+let i = 0;
+var ScoreTotal = [];
 
+//Questions array
+var Q1 = 'A variable that has not been assigned a value yet?';
+var Q2 = 'A collection of properties?';
+var Q3 = 'This will continue to run as long as condition is true?';
+var Q4 = 'What needs to be at the end of most javascript lines?';
+var Q5 = 'True or false statement?';
+var Questions = [ Q1, Q2, Q3, Q4, Q5 ];
+
+//Answers array
+
+var A1 = [ 'string', 'undefined', 'null' ];
+var A2 = [ 'object', 'symbol', 'text box' ];
+var A3 = [ 'string variable', 'for loop', 'function' ];
+var A4 = [ 'semicolon', 'colon', 'happy face' ];
+var A5 = [ 'equation', 'Boulean', 'expression' ];
+var Answers = [ A1, A2, A3, A4, A5 ];
+
+// Solutions array
+
+var S1 = 'Undefined is the correct answer';
+var S2 = 'Object is the correct answer';
+var S3 = 'For Loop is the correct answer';
+var S4 = 'semicolon is the correct answer, but you should always have a happy face with javascript';
+var S5 = 'Boulean is the correct answer';
+var Solutions = [ S1, S2, S3, S4, S5 ];
+
+// Correct answers - score
+
+var SC1 = [ 0, 1, 0 ];
+var SC2 = [ 1, 0, 0 ];
+var SC3 = [ 0, 1, 0 ];
+var SC4 = [ 1, 0, 0 ];
+var SC5 = [ 0, 1, 0 ];
+var Score = [ SC1, SC2, SC3, SC4, SC5 ];
+
+// function to start timer
 function startTimer(duration, display) {
 	var timer = duration,
 		minutes,
@@ -19,14 +58,14 @@ function startTimer(duration, display) {
 	}, 1000);
 }
 
+// Starts the timer once the page loads
 window.onload = function() {
 	var twoMinutes = 60 * 2,
 		display = document.querySelector('#time');
 	startTimer(twoMinutes, display);
 };
 
-var i = 0;
-
+// Question functions
 function NextQu() {
 	i++;
 	document.getElementById('intro').style.display = 'none';
@@ -50,6 +89,7 @@ function NextQu() {
 	document.getElementById('Next').style.display = 'none';
 }
 
+// function that runs when the Questions are finished
 function FinishQu() {
 	document.getElementById('Rolling Score').style.display = 'inline';
 
@@ -61,15 +101,17 @@ function FinishQu() {
 	document.getElementById('CurrentQuScore').style.display = 'none';
 	document.getElementById('Finish').style.display = 'none';
 
+	// here i stopped the timer when the user clicks on finish quiz button
+
 	clearTimeout(myTimer);
 	let endTime = document.getElementById('time').textContent;
-	let userEndTime = document.getElementById('endTime').textContent;
-	//let userEndTime = endTime;
-	console.log(userEndTime);
+	console.log(endTime);
+	// I need to figure out how to take this endTime variable results and combine
+	// with userName for High Scores list??
 }
 
-var ScoreTotal = [];
-
+//answer functions
+// I need help figuring out how to alert user on wrong answers and subtract 10 secs from the timer
 function Answer1Select() {
 	if (i < Questions.length - 1) {
 		j = 0;
@@ -153,50 +195,11 @@ function Answer3Select() {
 	}
 }
 
-//Questions are set up in an "array", the square brackets with Q1, Q2 being the questions which we can amend each time.
-var Q1 = 'A variable that has not been assigned a value yet?';
-var Q2 = 'A collection of properties?';
-var Q3 = 'This will continue to run as long as condition is true?';
-var Q4 = 'What needs to be at the end of most javascript lines?';
-var Q5 = 'True or false statement?';
-var Questions = [ Q1, Q2, Q3, Q4, Q5 ];
-
-//In a very similar way,  include our answers
-
-var A1 = [ 'string', 'undefined', 'null' ];
-var A2 = [ 'object', 'symbol', 'text box' ];
-var A3 = [ 'string variable', 'for loop', 'function' ];
-var A4 = [ 'semicolon', 'colon', 'happy face' ];
-var A5 = [ 'equation', 'Boulean', 'expression' ];
-var Answers = [ A1, A2, A3, A4, A5 ];
-
-//...and solutions
-
-var S1 = 'Undefined is the correct answer';
-var S2 = 'Object is the correct answer';
-var S3 = 'For Loop is the correct answer';
-var S4 = 'semicolon is the correct answer, but you should always have a happy face with javascript';
-var S5 = 'Boulean is the correct answer';
-var Solutions = [ S1, S2, S3, S4, S5 ];
-
-//...and points scored for each answer - we will keep it to 1s and 0s but you can be creative!
-
-var SC1 = [ 0, 1, 0 ];
-var SC2 = [ 1, 0, 0 ];
-var SC3 = [ 0, 1, 0 ];
-var SC4 = [ 1, 0, 0 ];
-var SC5 = [ 0, 1, 0 ];
-var Score = [ SC1, SC2, SC3, SC4, SC5 ];
-
-//We will set our first questions, answers etc. up to appear when the page loads
+function getSum(a, b) {
+	return a + b;
+}
 
 document.getElementById('QuestionQuiz').innerHTML = 'Question 1:' + Questions[i];
 document.getElementById('Answer1').innerHTML = Answers[i][0];
 document.getElementById('Answer2').innerHTML = Answers[i][1];
 document.getElementById('Answer3').innerHTML = Answers[i][2];
-
-//document.getElementById("Solution").innerHTML=Solutions[i];
-
-function getSum(a, b) {
-	return a + b;
-}
